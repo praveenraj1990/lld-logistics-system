b1199815fc75af0db8c3032abe3028b8ad27ac35
Prince <praveen0raj@gmail.com>
2022-01-24 19:39:56 +0530

    Adding initial changes

---
+++ src/main/java/com/logistics/lldlogisticssystem/LldLogisticsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.lldlogisticssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldLogisticsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldLogisticsSystemApplication.class, args);
+    }
+
+}
